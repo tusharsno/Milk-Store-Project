fb804b38f6ea0af887fa9e18268e44e0d8dbf953
Tushar Barua <tusharbarua269@gmail.com>
2025-12-25 17:39:00 +0600

    fix error

--- app/page.tsx
+++ app/page.tsx
@@ -17,16 +17,19 @@ export default function Page() {
 
       <section className="py-10 px-6 grid grid-cols-1 md:grid-cols-3 gap-6 max-w-6xl mx-auto">
         <ProductCard 
+          id={1}
           title="Fresh Cow Milk"
           price="70 BDT / L"
           img="/milk1.jpg"
         />
         <ProductCard 
+          id={2}
           title="Buffalo Milk"
           price="90 BDT / L"
           img="/milk2.jpg"
         />
         <ProductCard 
+          id={3}
           title="Ghee (Clarified Butter)"
           price="850 BDT"
           img="/ghee.jpg"
